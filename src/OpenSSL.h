33130e09370cef5f754f1c75e0000b72394fa013
Remy <git@relst.nl>
2023-03-18 19:36:21 +0100

    sonarqube test

--- src/OpenSSL.h
+++ src/OpenSSL.h
@@ -13,14 +13,46 @@
 #include <filesystem>
 #include <functional>
 
+struct OpenSSLFree
+{
+    void operator() (BIO* bio) const
+    { BIO_free(bio); }
 
-using X509_uptr = std::unique_ptr<X509, decltype(&::X509_free)>;
-using BIO_MEM_uptr = std::unique_ptr<BIO, decltype(&::BIO_free)>;
-using EVP_PKEY_uptr = std::unique_ptr<EVP_PKEY, decltype(&::EVP_PKEY_free)>;
-using BUF_MEM_uptr = std::unique_ptr<BUF_MEM, decltype(&::BUF_MEM_free)>;
-using X509_STORE_CTX_uptr = std::unique_ptr<X509_STORE_CTX, decltype(&::X509_STORE_CTX_free)>;
-using X509_STORE_uptr = std::unique_ptr<X509_STORE, decltype(&::X509_STORE_free)>;
-using X509_VERIFY_PARAM_uptr = std::unique_ptr<X509_VERIFY_PARAM, decltype(&::X509_VERIFY_PARAM_free)>;
+    void operator() (X509* x509) const
+    { X509_free(x509); }
+
+    void operator() (X509_STORE* store) const
+    { X509_STORE_free(store); }
+
+    void operator() (X509_STORE_CTX* ctx) const
+    { X509_STORE_CTX_free(ctx); }
+
+    void operator() (X509_VERIFY_PARAM* param) const
+    { X509_VERIFY_PARAM_free(param); }
+
+    void operator() (STACK_OF(X509)* st) const
+    { sk_X509_free(st); }
+
+    void operator() (GENERAL_NAME* gn) const
+    {GENERAL_NAME_free(gn); }
+
+    void operator() (STACK_OF(GENERAL_NAME)* st) const
+    { sk_GENERAL_NAME_free(st); }
+
+    void operator() (EVP_PKEY* evp_pkey) const
+    { EVP_PKEY_free(evp_pkey); }
+};
+
+using X509_uptr = std::unique_ptr<X509, OpenSSLFree>;
+using BIO_MEM_uptr = std::unique_ptr<BIO, OpenSSLFree>;
+using EVP_PKEY_uptr = std::unique_ptr<EVP_PKEY, OpenSSLFree>;
+using BUF_MEM_uptr = std::unique_ptr<BUF_MEM,OpenSSLFree>;
+using X509_STORE_CTX_uptr = std::unique_ptr<X509_STORE_CTX, OpenSSLFree>;
+using X509_STORE_uptr = std::unique_ptr<X509_STORE, OpenSSLFree>;
+using X509_VERIFY_PARAM_uptr = std::unique_ptr<X509_VERIFY_PARAM, OpenSSLFree>;
+using GENERAL_NAME_uptr = std::unique_ptr<GENERAL_NAME, OpenSSLFree>;
+using STACK_OF_GENERAL_NAMES_uptr = std::unique_ptr<STACK_OF(GENERAL_NAME), OpenSSLFree>;
+
 
 inline static const int maxKeySize = 4096;
 
@@ -100,10 +132,38 @@ public:
      * @param x509 OpenSSL X509 struct filled with certificate.
      * @return Contents of subject field of certificate, empty on error.
      */
-    static std::string x509_subject (const X509* x509) ;
+    static std::string x509_subject (const X509* x509);
 
+
+    /*
+     * Parses the X509* and returns the subjectAlternativeNames
+     * @param x509 OpenSSL X509 struct filled with certificate
+     * @return vector of strings filled with subjectAlternativeName
+     * NOTE: only parses DNS:. Not IP: or others.
+     */
+    static std::vector<std::string> x509_subject_alternative_dns_names(const X509* x509);
+
+    /**
+    * Parses the X509* and returns the subjectAlternativeNames
+    * @param x509 OpenSSL X509 struct filled with certificate
+    * @return vector of strings filled with subjectAlternativeName
+    * NOTE: only parses IP:. Not DNS: or others.
+    */
+    static std::vector<std::string> x509_subject_alternative_ip_names(const X509* x509);
 
 
     [[nodiscard]] static std::vector<X509_uptr> certs_to_x509(const std::string& certs_pem);
+
+
+private:
+
+    /**
+     * Used as a base class to convert subject or issuer to std::string,
+     * handles allocation and conversion.
+     * @param X509_X_NAME_FUNC Lambda that calls for example X509_get_subject_name.
+     * @return
+     */
+    static std::string x509_name_base(const X509 *const x509,
+                                      const std::function<void(const X509 *, const BIO_MEM_uptr &)> &X509_X_NAME_FUNC);
 };
 
